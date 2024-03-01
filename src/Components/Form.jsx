d8e10cc0f73aaa7a77a1a96d0f7ff2fea52b2e1e
Gagan-Raghuwanshi <raghu1cgagan@gmail.com>
2024-03-01 20:25:27 +0530

    first commit

---
+++ src/Components/Form.jsx
@@ -0,0 +1,106 @@
+import React, { useState } from "react";
+import { MdDriveFolderUpload } from "react-icons/md";
+
+function Form() {
+    const submitform = (e)=>{
+        e.preventDefault();
+        alert("Your Application Submitted ! successfully")
+    }
+
+    const [file,setFile] = useState();
+  let uploadfile = (e) => {
+    setFile(e.target.files[0])
+    console.log("file uploaded")
+  };
+  return (
+    <div className="border-2 w-[90%]  m-auto shadow-2xl mt-4">
+      <h1 className="text-center text-3xl font-bold m-auto mt-4">
+        Apply for this Job
+      </h1>
+      <hr className=" mt-8" />
+      <div className=" mx-20 mt-4">
+        <div className="">
+          <label htmlFor="" className="font-semibold">
+            First Name :
+          </label>{" "}
+          <br />
+          <input
+            type="text"
+            className="border-2 mt-2 w-full outline-none p-1 px-3 items-center rounded-md"
+          />
+        </div>
+        <div className="mt-4">
+          <label htmlFor="" className="font-semibold">
+            Middle Name :
+          </label>
+          <br />
+          <input
+            type="text"
+            className="border-2 mt-2 w-full outline-none p-1 px-3 items-center rounded-md"
+          />
+        </div>
+        <div className="mt-4">
+          <label htmlFor="" className="font-semibold">
+            Last Name :
+          </label>
+          <br />
+          <input
+            type="text"
+            className="border-2 mt-2 w-full outline-none p-1 px-3 items-center rounded-md"
+          />
+        </div>
+        <div className="mt-4">
+          <label htmlFor="" className="font-semibold">
+            Mobile No. :
+          </label>
+          <br />
+          <input
+            type="number"
+            className="border-2 mt-2 w-full outline-none p-1 px-3 items-center rounded-md"
+          />
+        </div>
+        <div className="mt-4">
+          <label htmlFor="" className="font-semibold">
+            Email Address :
+          </label>
+          <br />
+          <input
+            type="email"
+            className="border-2 mt-2 w-full outline-none p-1 px-3 items-center rounded-md"
+          />
+        </div>
+        <div
+          className=" border-2 max-w-max mt-7 flex flex-col items-center p-2 rounded-md cursor-pointer"
+          onClick={uploadfile}
+        >
+          <MdDriveFolderUpload className=" w-32 h-10" />
+          <label htmlFor="" className=" cursor-pointer font-semibold">
+            Upload Resume
+          </label>
+        </div>
+        <div className="mt-4">
+          <label htmlFor="" className="font-semibold">
+            Cover Letter :
+          </label>
+          <br />
+          <textarea
+            cols="50"
+            rows="5"
+            placeholder="Write Here"
+            className=" border-2 outline-none w-full p-3 px-5 rounded-md"
+          ></textarea>
+        </div>
+        <div className="flex justify-center m-6">
+          <input
+            type="submit"
+            value="Submit"
+            className=" bg-blue-700 text-white px-5 text-lg m-2 rounded-full cursor-pointer p-1"
+            onClick={submitform}
+          />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Form;
